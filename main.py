13ff1a374591a189993cb67ec0e04066d76a086e
1chooo <hugo970217@gmail.com>
2023-08-13 19:47:26 +0800

    separate main.py, Monster/app

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+Create Date: 2023/08/13
+Author: @1chooo(Hugo ChunHo Lin)
+Version: v0.0.2
+'''
+
+from Monster.app.app import start_flask
+
+def main() -> None:
+    start_flask()
+
+if __name__ == '__main__':
+    main()
